db9fd55a655bf5786bd386021e2d1cacc2b58d4d
Siya <siajabbarova@gmail.com>
2024-10-23 12:45:35 +0400

    add controllers

---
+++ EmployeeManagement.WebAPI/Controllers/CompanyController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CompanyController: ControllerBase
+{
+    
+}
---
+++ EmployeeManagement.WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DepartmentController: ControllerBase
+{
+    
+}
